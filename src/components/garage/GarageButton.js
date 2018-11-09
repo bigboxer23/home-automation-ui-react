b50e5a2480d1f61dc74146da35d09fd376631157
Matt Jones <bigboxer23@gmail.com>
2018-11-09 08:35:04 -0600

    tweak for not crashing load of page if garage node is not up

--- src/components/garage/GarageButton.js
+++ src/components/garage/GarageButton.js
@@ -14,7 +14,7 @@ class GarageButton extends React.Component
 	{
 		return <Button onClick={() => this.props.handleGarageClick(GarageButton.isDoorOpen(this.props.room) ? "Close" : "Open")} bsStyle={this.getButtonStyle()} bsSize="large" className={"m-2 position-relative d-flex justify-content-center"}>
 			<i className="mdi mdi-garage"/>
-			<div className="tempDisplay pr-1 pl-1 position-absolute" style={getTempStyle(GarageButton.findTemperature(this.props.room).temperature)} onClick={(event) => this.props.handleGarageMoreClick(event)}>{getFormattedTemp(GarageButton.findGarageDevice(this.props.room).temperature)}</div>
+			<div className="tempDisplay pr-1 pl-1 position-absolute" style={getTempStyle(GarageButton.findGarageTemperature(this.props.room))} onClick={(event) => this.props.handleGarageMoreClick(event)}>{getFormattedTemp(GarageButton.findGarageTemperature(this.props.room))}</div>
 			<div className="autoClose">{GarageButton.getAutoClose(this.props.room)}</div>
 			<div className="position-absolute bottom w-100 m-2 pl-2 pr-2">{this.props.room.name}</div>
 		</Button>;
@@ -30,10 +30,10 @@ class GarageButton extends React.Component
 		return room == null ? null : room.devices.find(theDevice => "Garage Opener" === theDevice.name);
 	}
 
-	static findTemperature(room)
+	static findGarageTemperature(room)
 	{
 		let aGarage = GarageButton.findGarageDevice(room);
-		return aGarage === null ? 100 : aGarage.temperature;
+		return aGarage == null ? 100 : aGarage.temperature;
 	}
 
 	static isDoorOpen(room)
