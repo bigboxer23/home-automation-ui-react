b75c6b1d3292bfc10b0b91325cc30b4b6625f01b
Matt Jones <bigboxer23@gmail.com>
2023-01-31 13:30:22 -0600

    add on/off toggle for Meural

--- src/components/meural/MeuralPageComponent.js
+++ src/components/meural/MeuralPageComponent.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderComponent from "../HeaderComponent"
 import MeuralNextButton from "./MeuralNextButton";
+import MeuralOnOffButton from "./MeuralOnOffButton";
 
-const MeuralPageComponent = ({back}) => (
+const MeuralPageComponent = ({back, device}) => (
 		<div>
 			<div className='background'></div>
 			<HeaderComponent back={back} name={"Meural Control"}/>
 			<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start room-content">
+				<MeuralOnOffButton key={device} device={device}/>
 				<MeuralNextButton key={"next"}/>
 			</div>
 		</div>
--- src/containers/MeuralPage.js
+++ src/containers/MeuralPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import {push} from "connected-react-router";
 import {bindActionCreators} from "redux";
-import {fetchStatusIfNecessary, sceneClicked} from '../actions'
+import {fetchStatusIfNecessary, sceneClicked, setMeuralOn} from '../actions'
 import MeuralPageComponent from "../components/meural/MeuralPageComponent";
 
 class MeuralPage extends React.Component
@@ -11,12 +11,38 @@ class MeuralPage extends React.Component
 	{
 		return <MeuralPageComponent {...this.props}/>
 	}
+
+	componentDidMount()
+	{
+		this.props.fetchStatus();
+	}
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+	device: findMeuralDeviceFromRooms (state.house.rooms)
+});
+
+const findMeuralDeviceFromRooms = (rooms) => {
+	return findMeuralDeviceFromRoom(rooms?.find(theRoom => "Meural" === theRoom.name));
+};
+
+export const findMeuralDeviceFromRoom = (room) => {
+	return room?.devices?.find(device => "Meural" === device.name);
+};
+
+export const getOnOffText = (devices) =>
+{
+	return isOn(devices) ? "Turn Off" : "Turn On";
+}
+
+export const isOn = (device) =>
+{
+	return "1.0" === device?.level;
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-	back: () => push('/')
+	back: () => push('/'),
+	fetchStatus: () => fetchStatusIfNecessary()
 }, dispatch);
 
 export default connect(
