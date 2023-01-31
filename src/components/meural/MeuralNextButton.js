a08db7b3e9aee0cc683e5cedd85f7b5262fa6206
Matt Jones <bigboxer23@gmail.com>
2023-01-31 14:00:43 -0600

    add button to go to next meural image

--- src/components/meural/MeuralNextButton.js
+++ src/components/meural/MeuralNextButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from 'react-bootstrap'
+import {nextMeuralImage} from "../../actions";
 
 const MeuralNextButton = props => (
 			<div>
-				<Button  variant="" size="lg" className={"mb-3 m-1 position-relative d-flex justify-content-center"}>
-					<i className="mdi mdi-lightbulb-group-outline"/>
+				<Button onClick={() => nextMeuralImage()} variant="" size="lg" className={"mb-3 m-1 position-relative d-flex justify-content-center"}>
+					<i className="mdi mdi-image-move"/>
 					<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">Next Image</div>
 				</Button>
 			</div>
