03082ba9f809249c45cc5bc421317e4bc0921fc6
Matt Jones <bigboxer23@gmail.com>
2023-01-30 19:39:47 -0600

    add background image instead of gradient colors (#55)

--- src/components/MainPageComponent.js
+++ src/components/MainPageComponent.js
@@ -2,9 +2,12 @@ import React from 'react'
 import {mapRoom} from "../containers/MainPage";
 
 const MainPageComponent = ({ time, rooms, handleClick, handleGarageClick, handleMoreClick, handleGarageMoreClick}) => (
-		<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start">{rooms.map(room =>
-				mapRoom(time, room, handleClick, handleGarageClick, handleMoreClick, handleGarageMoreClick)
-		)}</div>
+		<div>
+			<div className='background'></div>
+			<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start">{rooms.map(room =>
+					mapRoom(time, room, handleClick, handleGarageClick, handleMoreClick, handleGarageMoreClick)
+			)}</div>
+		</div>
 );
 
 export default MainPageComponent
