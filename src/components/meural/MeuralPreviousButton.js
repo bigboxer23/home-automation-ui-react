5eabf6267622373f8343229ac3a25e6f3aa3e73a
Matt Jones <bigboxer23@gmail.com>
2023-02-01 15:26:09 -0600

    adding snackbars to next/previous meural buttons

--- src/components/meural/MeuralPreviousButton.js
+++ src/components/meural/MeuralPreviousButton.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { Button } from 'react-bootstrap'
 import {previousMeuralImage} from "../../actions";
+import {Snackbar} from "@mui/material";
+import MuiAlert from '@mui/material/Alert';
 
-const MeuralPreviousButton = props => (
-		<div>
-			<Button onClick={() => previousMeuralImage()} variant="" size="lg" className={shouldDisplay(props.device?.status)}>
-				<i className="mdi mdi-image-move flip-horizontal"/>
-				<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">Previous Image</div>
-			</Button>
-		</div>
-);
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
+export default function MeuralPreviousButton(props) {
+	const [open, setOpen] = React.useState(false);
 
-const shouldDisplay = (status) => {
-	return "m-1 position-relative d-flex justify-content-center" + ("1" === status ? " d-none" : "");
+	const handleClick = () => {
+		setOpen(true);
+		previousMeuralImage();
+	};
+
+	const shouldDisplay = (status) => {
+		return "m-1 position-relative d-flex justify-content-center" + ("1" === status ? " d-none" : "");
+	}
+
+	return (
+			<div>
+				<Button onClick={handleClick} variant="" size="lg" className={shouldDisplay(props.device?.status)}>
+					<i className="mdi mdi-image-move flip-horizontal"/>
+					<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">Previous Image</div>
+				</Button>
+				<Snackbar open={open} autoHideDuration={3000} onClose={() => setOpen(false)}>
+					<Alert onClose={() => setOpen(false)} severity="info" sx={{ width: '100%' }}>
+						Fetching Previous Image
+					</Alert>
+				</Snackbar>
+			</div>
+	);
 }
-export default MeuralPreviousButton;
