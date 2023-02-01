87f1371f23a803258ef851130e8cc2ac86192bcd
Matt Jones <bigboxer23@gmail.com>
2023-02-01 15:26:09 -0600

    adding snackbars to next/previous meural buttons

--- src/components/meural/MeuralNextButton.js
+++ src/components/meural/MeuralNextButton.js
@@ -1,22 +1,44 @@
 import React from "react";
 import { Button } from 'react-bootstrap'
 import {nextMeuralImage} from "../../actions";
+import {Snackbar} from "@mui/material";
+import MuiAlert from '@mui/material/Alert';
 
-const MeuralNextButton = props => (
-			<div>
-				<Button onClick={() => nextMeuralImage()} variant="" size="lg" className={"mb-3 m-1 position-relative d-flex justify-content-center"}>
-					<i className={getIconStyle(props.device?.status)}/>
-					<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">{generateText(props.device?.status)}</div>
-				</Button>
-			</div>
-);
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
-const generateText = (status) => {
-	return ("1" === status ? "Generate New Image" : "Next Image");
+export default function MeuralNextButton(props) {
+	const [open, setOpen] = React.useState(false);
+
+	const handleClick = () => {
+		setOpen(true);
+		nextMeuralImage();
+	};
+
+	const generateButtonText = (status) => {
+		return ("1" === status ? "Generate New Image" : "Next Image");
+	};
+
+	const generateAlertText = (status) => {
+		return ("1" === status ? "Started Generating New Image" : "Fetching Next Image");
+	};
+
+	const getIconStyle = (status) => {
+		return "mdi mdi-image-" + ("1" === status ? "refresh-outline" : "move");
+	};
+
+	return (
+				<div>
+					<Button onClick={handleClick} variant="" size="lg" className={"mb-3 m-1 position-relative d-flex justify-content-center"}>
+						<i className={getIconStyle(props.device?.status)}/>
+						<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">{generateButtonText(props.device?.status)}</div>
+					</Button>
+					<Snackbar open={open} autoHideDuration={3000} onClose={() => setOpen(false)}>
+						<Alert onClose={() => setOpen(false)} severity="info" sx={{ width: '100%' }}>
+							{generateAlertText(props.device?.status)}
+						</Alert>
+					</Snackbar>
+				</div>
+	);
 }
-
-const getIconStyle = (status) => {
-	return "mdi mdi-image-" + ("1" === status ? "refresh-outline" : "move");
-}
-
-export default MeuralNextButton;
