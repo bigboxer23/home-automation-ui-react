cc959c8b4bf7e16a12168b883268ad7e9b283dac
Matt Jones <bigboxer23@gmail.com>
2023-01-31 22:09:18 -0600

    add meural source button

--- src/components/meural/MeuralPageComponent.js
+++ src/components/meural/MeuralPageComponent.js
@@ -3,12 +3,14 @@ import HeaderComponent from "../HeaderComponent"
 import MeuralNextButton from "./MeuralNextButton";
 import MeuralOnOffButton from "./MeuralOnOffButton";
 import MeuralPreviousButton from "./MeuralPreviousButton";
+import MeuralSourceButton from "./MeuralSourceButton";
 
 const MeuralPageComponent = ({back, device}) => (
 		<div>
 			<div className='background'></div>
 			<HeaderComponent back={back} name={"Meural Control"}/>
 			<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start room-content">
+				<MeuralSourceButton device={device}/>
 				<MeuralOnOffButton key={device} device={device}/>
 				<MeuralPreviousButton key={"previous"}/>
 				<MeuralNextButton key={"next"}/>
