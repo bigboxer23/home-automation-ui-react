7796fd3f3b74f6ac326164397446d715b7faf2df
Matt Jones <bigboxer23@gmail.com>
2023-02-04 12:33:17 -0600

    add display of active openAI prompt to meural page

--- src/components/meural/MeuralPageComponent.js
+++ src/components/meural/MeuralPageComponent.js
@@ -5,6 +5,7 @@ import MeuralOnOffButton from "./MeuralOnOffButton";
 import MeuralPreviousButton from "./MeuralPreviousButton";
 import MeuralSourceButton from "./MeuralSourceButton";
 import MeuralGenerateImageButton from "./MeuralGenerateImageButton";
+import MeuralPromptDisplay from "./MeuralPromptDisplay";
 
 const MeuralPageComponent = ({back, device}) => (
 		<div>
@@ -12,6 +13,7 @@ const MeuralPageComponent = ({back, device}) => (
 			<HeaderComponent back={back} name={"Meural Control"}/>
 			<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start room-content">
 				<MeuralSourceButton device={device}/>
+				<MeuralPromptDisplay device={device}/>
 				<MeuralOnOffButton key={device} device={device}/>
 				<MeuralPreviousButton key={"previous"} device={device}/>
 				<MeuralNextButton key={"next"} device={device}/>
---
+++ src/components/meural/MeuralPromptDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MeuralPromptDisplay(props) {
+
+	const getClassnames = (status) => {
+		return "m-1 position-relative d-flex flex-column justify-content-center" + ("0" === status ? " d-none" : "");
+	}
+
+	return (
+			<div className={getClassnames(props.device?.status)}>
+				<div className={"ms-3 meural-source-button-label"}>Active Prompt</div>
+				<div className={"MuiToggleButtonGroup-root p-3 meural-source-button-label"}>{props.device?.temperature}</div>
+			</div>
+	);
+}
