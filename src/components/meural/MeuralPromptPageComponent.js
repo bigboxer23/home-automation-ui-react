2b4d50880c8848798f907582886ce12f1f65f019
Matt Jones <bigboxer23@gmail.com>
2023-02-17 11:44:28 -0600

    add initial page for meural prompt

---
+++ src/components/meural/MeuralPromptPageComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import HeaderComponent from "../HeaderComponent";
+import { TextField } from "@mui/material";
+import { Button } from "react-bootstrap";
+
+const MeuralPromptPageComponent = ({ back, handleKeyUp, handleClick }) => (
+	<div>
+		<div className="background"></div>
+		<HeaderComponent back={back} name={"Meural Control"} />
+		<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start room-content">
+			<div className={"w-100 d-flex flex-column"}>
+				<div className={"ps-3 pe-3 mb-2 MuiToggleButtonGroup-root"}>
+					<TextField
+						onKeyUp={handleKeyUp}
+						autoFocus
+						margin="normal"
+						id="creationPrompt"
+						label="Enter prompt to generate image from"
+						type="text"
+						fullWidth
+						variant="standard"
+						className={"pb-2"}
+					/>
+				</div>
+				<Button onClick={handleClick}>Create Image</Button>
+			</div>
+		</div>
+	</div>
+);
+
+export default MeuralPromptPageComponent;
