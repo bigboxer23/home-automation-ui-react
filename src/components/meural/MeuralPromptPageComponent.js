68425fe3fb112310a5a443c7efd8407fe7713124
Matt Jones <bigboxer23@gmail.com>
2023-04-20 10:36:18 -0500

    Use textarea so longer prompts don't run out of space and scroll

--- src/components/meural/MeuralPromptPageComponent.js
+++ src/components/meural/MeuralPromptPageComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HeaderComponent from "../HeaderComponent";
-import { TextField } from "@mui/material";
+import { TextareaAutosize } from "@mui/material";
 import { Button } from "react-bootstrap";
 
 const MeuralPromptPageComponent = ({ back, handleKeyUp, handleClick }) => (
@@ -10,16 +10,16 @@ const MeuralPromptPageComponent = ({ back, handleKeyUp, handleClick }) => (
 		<div className="p-2 w-100 h-100 d-flex flex-wrap justify-content-center align-content-start room-content">
 			<div className={"w-100 d-flex flex-column"}>
 				<div className={"ps-3 pe-3 mb-2 MuiToggleButtonGroup-root"}>
-					<TextField
+					<TextareaAutosize
 						onKeyUp={handleKeyUp}
 						autoFocus
 						margin="normal"
 						id="creationPrompt"
-						label="Enter prompt to generate image from"
+						placeholder="Enter prompt to generate image from"
 						type="text"
 						fullWidth
 						variant="standard"
-						className={"pb-2"}
+						className={"pt-4 w-100 prompt-textarea"}
 					/>
 				</div>
 				<Button onClick={handleClick}>Create New Image</Button>
