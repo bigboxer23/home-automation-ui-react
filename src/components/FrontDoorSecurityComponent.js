e0ad10e8402eb0417ae2aa6b0d6c95ab058232e0
Matt Jones <bigboxer23@gmail.com>
2023-04-25 13:05:32 -0500

    update security page to properly size the content returned from the motion server's stream.

--- src/components/FrontDoorSecurityComponent.js
+++ src/components/FrontDoorSecurityComponent.js
@@ -1,14 +1,15 @@
 import React from "react";
 import HeaderComponent from "./HeaderComponent";
 
-const FrontDoorSecurityComponent = ({ back }) => (
+const FrontDoorSecurityComponent = ({ back, load }) => (
 	<div>
 		<div className="background"></div>
 		<HeaderComponent back={back} name={"Front Door Security"} />
 		<iframe
 			title={"FrontDoorSecurity"}
-			className={"security"}
+			className={"security room-content"}
 			src={"/FrontDoor"}
+			ref={load}
 		></iframe>
 	</div>
 );
--- src/containers/FrontDoorSecurityPage.js
+++ src/containers/FrontDoorSecurityPage.js
@@ -10,12 +10,32 @@ class FrontDoorSecurityPage extends React.Component {
 	}
 }
 
+let intervalId = null;
+
+const initializeIframe = function (iframe) {
+	if (intervalId == null) {
+		intervalId = setInterval(resizeImgContent, 300, iframe);
+	}
+};
+
+const resizeImgContent = function (iframe) {
+	if (iframe.contentDocument == null) {
+		clearInterval(intervalId);
+		intervalId = null;
+		return;
+	}
+	if (iframe.contentDocument.getElementsByTagName("img").length > 0) {
+		iframe.contentDocument.getElementsByTagName("img")[0].style = "width:100%";
+	}
+};
+
 const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) =>
 	bindActionCreators(
 		{
 			back: () => push("/"),
+			load: (o) => (dispatch) => initializeIframe(o),
 		},
 		dispatch
 	);
