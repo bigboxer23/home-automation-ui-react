d32d3d0173ccaa56b0b2d572de65529036427afb
Matt Jones <bigboxer23@gmail.com>
2023-09-27 20:10:42 -0500

    added specific icons for scenes

--- src/components/front_porch/FrontPorchColorButton.js
+++ src/components/front_porch/FrontPorchColorButton.js
@@ -10,7 +10,7 @@ const FrontPorchColorButton = (props) => (
 			size="lg"
 			className={"mb-3 m-1 position-relative d-flex justify-content-center"}
 		>
-			<i className="mdi mdi-lightbulb-group-outline" />
+			<i className={getIconStyle(props.device.name)} />
 			<div className="position-absolute bottom w-100 m-2 ps-2 pe-2">
 				{props.device.name}
 			</div>
@@ -18,6 +18,27 @@ const FrontPorchColorButton = (props) => (
 	</div>
 );
 
+const getIconStyle = function (name) {
+	//Pride
+	const mdi = "mdi ";
+	switch (name) {
+		case "Pride":
+			return mdi + "mdi-looks";
+		case "Christmas":
+			return mdi + "mdi-forest";
+		case "Valentines":
+			return mdi + "mdi-heart-outline";
+		case "Halloween":
+			return mdi + "mdi-halloween";
+		case "Thanksgiving":
+			return mdi + "mdi-turkey";
+		case "Retro":
+			return mdi + "mdi-lamps";
+		case "Normal":
+			return mdi + "mdi-lightbulb-group-off-outline";
+	}
+	return mdi + "mdi-lightbulb-group-outline";
+};
 const getButtonStyle = function (device) {
 	return isDeviceOn(device) ? "success" : "default";
 };
