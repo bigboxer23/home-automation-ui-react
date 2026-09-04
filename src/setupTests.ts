16fd8074f426d5e939667af7d7d9f990b092f3a9
Matt Jones <bigboxer23@gmail.com>
2026-09-04 06:07:02 -0500

    chore: bumping major deps

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 
 // Wrap native Request to support relative URLs in jsdom/node environment
 const OriginalRequest = globalThis.Request;
